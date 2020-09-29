5b70930ab18b5ba8f779a1693f9696372836d47f
CHENYANG FANG <fang524676125@outlook.com>
2020-09-29 16:43:25 -0700

    add integration test

---
+++ hw1/src/test/java/integration/PlayGameTest.java
@@ -0,0 +1,23 @@
+package integration;
+
+import models.GameBoard;
+import models.Player;
+import models.Move;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.TestMethodOrder;
+
+import org.junit.jupiter.api.Test;
+
+@TestMethodOrder(OrderAnnotation.class)
+class PlayGameTest {
+
+	@Test
+	void test() {
+		
+	}
+
+}
